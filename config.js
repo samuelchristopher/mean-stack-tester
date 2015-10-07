d0a4439cd0a94a62f01fb6f6157c8a30d143cf56
Samuel Christopher <thesamchris@gmail.com>
2015-10-07 10:16:17 +0100

    Created a database

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "database": "",
+  "database": "mongodb://root:root@ds047722.mongolab.com:47722/site",
   "port": process.env.PORT || 3000,
   "secretKey": "dksjfdusdklfhe89r7ti094890dfgjgkjfjgklsjg7t7596783hjhsdflk"
 }
