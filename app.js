3ef865edd758f1745d27ec2f56cbee370b2a1a67
Samuel Christopher <thesamchris@gmail.com>
2015-10-07 15:36:58 +0100

    Setup the front end files

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var morgan = require('morgan');
-var config = require('./config');
-var mongoose = require('mongoose');
+var express     = require('express');
+var bodyParser  = require('body-parser');
+var morgan      = require('morgan');
+var config      = require('./config');
+var mongoose    = require('mongoose');
 
 var app = express();
 
@@ -18,11 +18,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
+app.use(express.static(__dirname + '/public'));
+
 var api = require('./app/routes/api')(app, express);
 app.use('/api', api);
 
 app.get('*', function(req, res) {
-  res.sendFile(__dirname + '/public/views/index.html');
+  res.sendFile(__dirname + '/public/app/views/index.html');
 });
 
 app.listen(config.port, function(err) {
