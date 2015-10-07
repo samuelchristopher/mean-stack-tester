c861db5be67e0b6304b2f7d949f9d2f8e1389347
Samuel Christopher <thesamchris@gmail.com>
2015-10-07 10:00:34 +0100

    Setting up

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var config = require('./config');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+app.listen(config.port, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Listening on port 3000');
+  }
+});
