94078a3dae751f988ea17aabaabd8b310606c0b5
Samuel Christopher <thesamchris@gmail.com>
2015-10-07 10:26:53 +0100

    Added password hashing

--- app/models/user.js
+++ app/models/user.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
 
 var Schema = mongoose.Schema;
 
@@ -6,6 +7,16 @@ var UserShema = new Schema({
   name: String,
   username: { type: String, required: true, index: { unique: true } },
   password: { type: String, required: true, select: false }
+});
+
+UserShema.pre('save', function(next) {
+  var user = this;
+  if(!user.isModified('password')) return next();
+  bcrypt.hash(user.password, null, null, function(err, hash) {
+    if(err) return next(err);
+    user.password = hash;
+    next();
+  });
 });
 
 module.exports = mongoose.model("User", UserShema);
