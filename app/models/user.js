3211c6b0c735c5001feb0e7f0121404a28f25cc7
Samuel Christopher <thesamchris@gmail.com>
2015-10-07 11:11:49 +0100

    Created login API

--- app/models/user.js
+++ app/models/user.js
@@ -3,13 +3,13 @@ var bcrypt = require('bcrypt-nodejs');
 
 var Schema = mongoose.Schema;
 
-var UserShema = new Schema({
+var UserSchema = new Schema({
   name: String,
   username: { type: String, required: true, index: { unique: true } },
   password: { type: String, required: true, select: false }
 });
 
-UserShema.pre('save', function(next) {
+UserSchema.pre('save', function(next) {
   var user = this;
   if(!user.isModified('password')) return next();
   bcrypt.hash(user.password, null, null, function(err, hash) {
@@ -19,9 +19,9 @@ UserShema.pre('save', function(next) {
   });
 });
 
-UserShema.method.comparePassword = function(password) {
+UserSchema.methods.checkPassword = function(password) {
   var user = this;
   return bcrypt.compareSync(password, user.password);
-}
+};
 
-module.exports = mongoose.model("User", UserShema);
+module.exports = mongoose.model("User", UserSchema);
--- app/routes/api.js
+++ app/routes/api.js
@@ -1,6 +1,19 @@
 var User        = require('../models/user');
 var config      = require('../../config');
 var secretKey   = config.secretKey;
+var jwt         = require('jsonwebtoken');
+
+function createToken(user) {
+  var token = jwt.sign({
+    _id: user.id,
+    name: user.name,
+    username: user.username
+  }, secretKey, {
+    expiresInMinute: 1440
+  });
+
+  return token;
+}
 
 module.exports = function(app, express) {
   var api = express.Router();
@@ -30,6 +43,31 @@ module.exports = function(app, express) {
       }
 
       res.json(users);
+    });
+  });
+
+  api.post('/login', function(req, res) {
+    User.findOne({
+      username: req.body.username
+    }).select('password').exec(function(err, user) {
+      if(err) throw err;
+      if (!user) {
+        res.send({ message: "User doesn't exist" });
+      } else if (user) {
+        var validPassword = user.checkPassword(req.body.password);
+
+        if (!validPassword) {
+          res.send({ message: "Invalid password" })
+        } else {
+          // token
+          var token = createToken(user);
+          res.json({
+            success: true,
+            message: "Successfully logged in!",
+            token: token
+          });
+        }
+      }
     });
   });
 
